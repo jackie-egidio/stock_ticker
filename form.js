4eebd89697d48f246553547d82ba88277c6471e8
jackie-egidio <52717215+jackie-egidio@users.noreply.github.com>
2022-04-24 15:34:11 -0400

    Create form.js

---
+++ form.js
@@ -0,0 +1,7 @@
+var url = require('url');
+var adr = 'http://localhost:stock_ticker.html';
+var q = url.parse(adr, true);
+
+console.log(q.host); //returns 'localhost:8080'
+console.log(q.pathname); //returns '/default.htm'
+console.log(q.search); //returns '?year=2017&month=february'
