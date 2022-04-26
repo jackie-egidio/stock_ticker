cd39c2451fb69cb879aa2229cf74f63858f48d6d
jackie-egidio <52717215+jackie-egidio@users.noreply.github.com>
2022-04-26 14:59:34 -0400

    Update form.js

--- form.js
+++ form.js
@@ -1,7 +1,23 @@
 var url = require('url');
-var adr = 'http://localhost:stock_ticker.html';
+var http = require('http');
+var fs = require('fs');
+var adr = 'https://jackie-egidio.github.io/stock_ticker/';
 var q = url.parse(adr, true);
 
-console.log(q.host); //returns 'localhost:8080'
-console.log(q.pathname); //returns '/default.htm'
-console.log(q.search); //returns '?year=2017&month=february'
+console.log(q.host); 
+console.log(q.pathname); 
+
+http.createServer(function (req, res) {
+    console.log("in createServer");
+  var q = url.parse(req.url, true);
+  var filename = "." + q.pathname;
+  fs.readFile(filename, function(err, data) {
+    if (err) {
+      res.writeHead(404, {'Content-Type': 'text/html'});
+      return res.end("404 Not Found");
+    } 
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write("hello");
+    return res.end();
+  });
+}).listen(8080);
